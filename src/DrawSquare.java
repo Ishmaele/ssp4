7d2080f80152b5546d62e44f5c0ad178fd654b33
Ishmaele <helghast50@gmail.com>
2019-04-24 12:42:53 +0300

    DrawRectangle finished

---
+++ src/DrawSquare.java
@@ -0,0 +1,53 @@
+import sun.plugin2.util.ColorUtil;
+
+import java.awt.*;
+import java.awt.Point;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DrawSquare extends JPanel{
+
+    MouseListen mouseListen = new MouseListen();
+    Point p1 = new Point(0, 0);
+   static Random rand = new Random();
+
+
+    int x = rand.nextInt(400);
+    int y = rand.nextInt(300);
+
+    public DrawSquare(){
+        this.setPreferredSize(new Dimension(500, 400));
+        this.addMouseListener(mouseListen);
+    }
+    public void paintComponent(Graphics g) {
+        Color myRandColor = new Color(rand.nextInt( 255),rand.nextInt( 255),rand.nextInt( 255));
+        int width = ThreadLocalRandom.current().nextInt(50, 100);
+        int height = ThreadLocalRandom.current().nextInt(50, 100);
+        g.drawRect(x, y, width, height);
+        g.setColor(myRandColor);
+        g.fillRect(x, y, width, height);
+    }
+
+    public class MouseListen extends MouseAdapter {
+        public void mouseClicked(MouseEvent e) {
+            p1 = e.getPoint();
+
+            if (p1.x>x && p1.x<(x+100) && p1.y>y && p1.y <(y+100))
+            {
+                repaint();
+                x = rand.nextInt(400);
+                y = rand.nextInt(300);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Draw Square");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(new DrawSquare());
+        f.pack();
+        f.setVisible(true);
+    }
+}
